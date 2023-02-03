8220a2b9ec29459297dcf2c17a71e16473ce72cb
lukas.hofer <20190016@students.htl-perg.ac.at>
2023-02-03 12:42:17 +0100

    Added Tab Control

--- DatabaseDocumentationGenerator/DatabaseDocumentationGenerator/Form1.Designer.cs
+++ DatabaseDocumentationGenerator/DatabaseDocumentationGenerator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DatabaseDocumentationGenerator
 {
-    partial class Form1
+    partial class DatabaseDocumentationGenerator
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.DataCatalog = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.DataCatalog);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(802, 449);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // DataCatalog
+            // 
+            this.DataCatalog.Location = new System.Drawing.Point(4, 24);
+            this.DataCatalog.Name = "DataCatalog";
+            this.DataCatalog.Padding = new System.Windows.Forms.Padding(3);
+            this.DataCatalog.Size = new System.Drawing.Size(794, 421);
+            this.DataCatalog.TabIndex = 0;
+            this.DataCatalog.Text = "DataCatalog";
+            this.DataCatalog.UseVisualStyleBackColor = true;
+            // 
+            // DatabaseDocumentationGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tabControl1);
+            this.Name = "DatabaseDocumentationGenerator";
+            this.Text = "DatabaseDocumentationGenerator";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage DataCatalog;
     }
 }
--- DatabaseDocumentationGenerator/DatabaseDocumentationGenerator/Form1.cs
+++ DatabaseDocumentationGenerator/DatabaseDocumentationGenerator/Form1.cs
@@ -1,8 +1,8 @@
 namespace DatabaseDocumentationGenerator
 {
-    public partial class Form1 : Form
+    public partial class DatabaseDocumentationGenerator : Form
     {
-        public Form1()
+        public DatabaseDocumentationGenerator()
         {
             InitializeComponent();
         }
